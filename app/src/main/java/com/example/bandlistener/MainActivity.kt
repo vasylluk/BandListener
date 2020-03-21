0de4d8e607579db1330751685b84246477ff4087
Vasyl Lukachynets <vasyl3luk@gmail.com>
2020-03-22 00:05:35 +0200

    drawable resources added & MainActivity modified with minor changes

--- app/src/main/java/com/example/bandlistener/MainActivity.kt
+++ app/src/main/java/com/example/bandlistener/MainActivity.kt
@@ -8,15 +8,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 
 
 class MainActivity : AppCompatActivity() {
 
     var navigationDrawerIconsList: ArrayList<String> = arrayListOf()
+    var images_for_navdrawer = intArrayOf(
+        R.drawable.navigation_allsongs, R.drawable.navigation_favorites,
+        R.drawable.navigation_settings, R.drawable.navigation_aboutus)
     var trackNotificationBuilder: Notification? = null
 
     object Statified {
@@ -32,13 +37,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+        Statified.drawerLayout = findViewById(R.id.drawer_layout)
+
         navigationDrawerIconsList.add("All Songs")
         navigationDrawerIconsList.add("Favourites")
         navigationDrawerIconsList.add("Settings")
         navigationDrawerIconsList.add("About Us")
 
+        val toggle = ActionBarDrawerToggle(this@MainActivity,
+            Statified.drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
+        Statified.drawerLayout?.addDrawerListener(toggle)
 
 
+        toggle.syncState()
 
 
 
@@ -67,5 +84,4 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
-
 }
