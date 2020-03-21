0853af280f7150f2131c254b3cd73b0e851e9d62
Vasyl Lukachynets <vasyl3luk@gmail.com>
2020-03-21 19:32:57 +0200

    made minor modifications of MainActivity

--- app/src/main/java/com/example/bandlistener/MainActivity.kt
+++ app/src/main/java/com/example/bandlistener/MainActivity.kt
@@ -1,12 +1,71 @@
 package com.example.bandlistener
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+
+    var navigationDrawerIconsList: ArrayList<String> = arrayListOf()
+    var trackNotificationBuilder: Notification? = null
+
+    object Statified {
+        var drawerLayout: DrawerLayout? = null
+        var notificationManager: NotificationManager? = null
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigationDrawerIconsList.add("All Songs")
+        navigationDrawerIconsList.add("Favourites")
+        navigationDrawerIconsList.add("Settings")
+        navigationDrawerIconsList.add("About Us")
+
+
+
+
+
+
+        val navigation_recycler_view = findViewById<RecyclerView>(R.id.navigation_recycler_view)
+        navigation_recycler_view.layoutManager = LinearLayoutManager(this)
+        navigation_recycler_view.itemAnimator = DefaultItemAnimator()
+        navigation_recycler_view.adapter = _navigationAdapter
+        navigation_recycler_view.setHasFixedSize(true)
+        val intent = Intent(this@MainActivity, MainActivity::class.java)
+        val pIntent = PendingIntent.getActivity(this@MainActivity, System.currentTimeMillis().toInt(), intent, 0)
+        trackNotificationBuilder = Notification.Builder(this)
+            .setContentTitle("A track is playing the background.")
+            .setSmallIcon(R.drawable.app_icon)
+            .setContentIntent(pIntent)
+            .setOngoing(true)
+            .setAutoCancel(true)
+            .build()
+        Statified.notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     }
+
+    override fun onStart() {
+        super.onStart()
+        try {
+            Statified.notificationManager?.cancel(1978)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
 }
